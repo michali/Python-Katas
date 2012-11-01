e30644fbe4efba648968db97deca6d3c89367701
Michali <michali@gmail.com>
2012-11-01 22:03:28 +0000

    Operator node with operator node

--- BinaryExpressionTree/src/Tests/NodeTests.py
+++ BinaryExpressionTree/src/Tests/NodeTests.py
@@ -16,13 +16,23 @@ class ValueNodeTests(unittest.TestCase):
 
 class OperatorNodeTests(unittest.TestCase):
     
-    def test_create_operator_node(self):
+    def test_evaluate_shallow_node(self):
         node = OperatorNode(lambda x,y: x * y)
         node.left = ValueNode(3)
         node.right = ValueNode(2)
         
         self.assertEqual(6, node.evaluate())
-
+        
+    def test_evaluate_deep_operator_node(self):
+        node = OperatorNode(lambda x,y: x * y)
+        left_node = OperatorNode(lambda x,y : x + y)
+        node.left = left_node
+        left_node.left = ValueNode(1)
+        left_node.right = ValueNode(2)
+        node.right = ValueNode(5)
+        
+        self.assertEqual(15, node.evaluate())
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
