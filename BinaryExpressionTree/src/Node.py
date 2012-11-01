93be8e9d0ec209f10b543d3f3122febf3f2fd956
Michali <michali@gmail.com>
2012-11-01 21:54:54 +0000

    Operator node

--- BinaryExpressionTree/src/Node.py
+++ BinaryExpressionTree/src/Node.py
@@ -11,3 +11,27 @@ class ValueNode:
         
     def evaluate(self):
         return self.___value
+
+class OperatorNode:
+    
+    def __init__(self, operation):
+        self.__operation = operation
+        
+    @property
+    def left(self):
+        pass
+
+    @left.setter
+    def left(self, node):
+        self.__left = node
+        
+    @property
+    def right(self):
+        pass
+    
+    @right.setter
+    def right(self, node):
+        self.__right = node
+    
+    def evaluate(self):
+        return self.__operation(self.__left.evaluate(), self.__right.evaluate())
--- BinaryExpressionTree/src/Tests/NodeTests.py
+++ BinaryExpressionTree/src/Tests/NodeTests.py
@@ -4,16 +4,24 @@ Created on 13 Oct 2012
 @author: Michali
 '''
 import unittest
+from Node import OperatorNode
 from Node import ValueNode
 
 class ValueNodeTests(unittest.TestCase):
 
-    def test_create_numerical_node(self):
+    def test_create_value_node(self):
         node = ValueNode(3)
         
-        self.assertEqual(3, node.evaluate())        
+        self.assertEqual(3, node.evaluate())       
 
-
+class OperatorNodeTests(unittest.TestCase):
+    
+    def test_create_operator_node(self):
+        node = OperatorNode(lambda x,y: x * y)
+        node.left = ValueNode(3)
+        node.right = ValueNode(2)
+        
+        self.assertEqual(6, node.evaluate())
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
