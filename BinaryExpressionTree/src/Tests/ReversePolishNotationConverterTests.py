9394172c1485fb5d1fde0c98acea0b0673c2056a
Michali <michali@gmail.com>
2013-03-19 22:27:00 +0000

    refactoring, and introducing multiple operators of different precedence

--- BinaryExpressionTree/src/Tests/ReversePolishNotationConverterTests.py
+++ BinaryExpressionTree/src/Tests/ReversePolishNotationConverterTests.py
@@ -18,12 +18,8 @@ class ReversePolishNotationConverter(object):
     def __initialise(self, expression):
         expWithVal = "" if not expression else expression
         self.__tokens = expWithVal.split()
-        self.__previousToken = ""
+        self.__previousTokens = []
         self.__result = ""
-        
-    def __handle_operator(self, current):
-        self.__append_token(self.__previousToken)
-        self.__previousToken = current
 
     def __process_tokens(self):
         for current in self.__tokens:
@@ -31,8 +27,16 @@ class ReversePolishNotationConverter(object):
                 self.__append_token(current)
             else:
                 self.__handle_operator(current)        
-        self.__append_token(self.__previousToken)  
-          
+        self.__append_previous_token()  
+    
+    def __append_previous_token(self):
+        if len(self.__previousTokens) > 0:
+            self.__append_token(self.__previousTokens.pop())
+     
+    def __handle_operator(self, current):
+        self.__append_previous_token()
+        self.__previousTokens.append(current)
+      
     def __append_token(self, token):
         if (len(self.__result) > 0 and len(token) > 0):
             self.__result += " "
@@ -62,7 +66,11 @@ class ReversePolishNotationConverterTests(unittest.TestCase):
     def test_HandlesMultipleOperatorsOfSamePrecedence(self):
         self.when("a - 5 + 3")
         self.then("a 5 - 3 +")
-        
+    
+    @unittest.skip("has not been implemented")    
+    def test_HandlesMultipleOperatorsOfDifferentPrecedence(self):
+        self.when("a - 5 * 3")
+        self.then("a 5 3 * -")
         
     def setUp(self):
         self.converter = ReversePolishNotationConverter()
