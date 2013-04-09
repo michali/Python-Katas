bca5b75f515adaa515fe679d1172fdcf646aa9f4
Michali <michali@gmail.com>
2013-04-09 01:36:15 +0100

    tested with multiple operators but no brackets

--- BinaryExpressionTree/src/NodeParser.py
+++ BinaryExpressionTree/src/NodeParser.py
@@ -4,25 +4,40 @@ Created on 4 Nov 2012
 @author: Michali
 '''
 from Node import OperatorNode, ValueNode
-
+import re
 
 class NodeParser():
-           
+    
+    def __init__(self, rpnConverter):
+        self.__rpnConverter = rpnConverter   
+    
     def parse(self, expression):
         rpn_exp = self.__convert_to_rpn(expression)
         buffer = []
-        for token in rpn_exp:
+        rpn_tokens = rpn_exp.split()
+        for token in rpn_tokens:
             if self.__is_number(token):
                 buffer.append(ValueNode(int(token)))
             else:
-                operator_node = self.__create_operator_node(token)
+                operator_node = OperatorNode(self.__create_operation(token))
                 operator_node.right = buffer.pop()
                 operator_node.left = buffer.pop()
-                buffer.append(operator_node)
+                buffer.append(operator_node)      
                 
+        return operator_node          
        
     def __convert_to_rpn(self, expression):
-        pass
+        return self.__rpnConverter.transform(expression)
     
-    def __create_operator_node(self, token):
-        pass
+    def __is_number(self, token):
+        return re.match("\d+", token) != None
+    
+    def __create_operation(self, token):
+        if token == "+":
+            return lambda x, y : x + y
+        if token == "-":
+            return lambda x, y : x - y
+        if token == "*":
+            return lambda x, y : x * y
+        return lambda x, y : x / y
+            
--- BinaryExpressionTree/src/Tests/NodeParserTests.py
+++ BinaryExpressionTree/src/Tests/NodeParserTests.py
@@ -5,43 +5,55 @@ Created on 4 Nov 2012
 '''
 import unittest
 from NodeParser import NodeParser
+from mock import Mock
 
 class NodeParserTests(unittest.TestCase):
 
     def setUp(self):
-        self.parser = NodeParser()
+        self.mock = Mock()
+        self.parser = NodeParser(self.mock)
 
     def test_parses_a_plus(self):
-        tree = self.parser.parse("1+2")
+        exp = "1+2"
+        self.mock.transform.return_value = "1 2 +"
+        tree = self.parser.parse(exp)
         
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.evaluate())
         self.assertEqual(3, tree.evaluate())
         
     def test_parses_a_minus(self):
-        tree = self.parser.parse("1-2")
+        exp = "1-2"
+        self.mock.transform.return_value = "1 2 -"
+        tree = self.parser.parse(exp)
         
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.evaluate())
         self.assertEqual(-1, tree.evaluate())
 
     def test_parses_a_multiplication(self):
-        tree = self.parser.parse("2*3")
+        exp = "2*3"
+        self.mock.transform.return_value = "2 3 *"
+        tree = self.parser.parse(exp)
         
         self.assertEqual(2, tree.left.evaluate())
         self.assertEqual(3, tree.right.evaluate())
         self.assertEqual(6, tree.evaluate())   
         
     def test_parses_a_division(self):
-        tree = self.parser.parse("1/2")
+        exp = "1/2"
+        self.mock.transform.return_value = "1 2 /"
+        tree = self.parser.parse(exp)
         
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.evaluate())
         self.assertEqual(0.5, tree.evaluate())     
         
     def test_parses_complex_no_brackets(self):
-        tree = self.parser.parse("1+2/8")
-               
+        exp = "1+2/8"
+        self.mock.transform.return_value = "1 2 8 / +"
+        tree = self.parser.parse(exp)
+           
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.left.evaluate())
         self.assertEqual(8, tree.right.right.evaluate())
