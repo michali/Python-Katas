ecc29f59a71ca77efd87f26f55dab0f5407f61ec
Michali Konstantinidis <michali@gmail.com>
2013-04-27 02:07:14 +0100

    Migrated to Mox

--- BinaryExpressionTree/src/Node.py
+++ BinaryExpressionTree/src/Node.py
@@ -7,7 +7,7 @@ Created on 14 Oct 2012
 class ValueNode:
 
     def __init__(self, value):
-        self.___value = value
+        self.___value = float(value)
         
     def evaluate(self):
         return self.___value
--- BinaryExpressionTree/src/Tests/NodeParserTests.py
+++ BinaryExpressionTree/src/Tests/NodeParserTests.py
@@ -5,23 +5,25 @@ Created on 4 Nov 2012
 '''
 import unittest
 from NodeParser import NodeParser
-from mock import Mock
+import mox
+from ReversePolishNotationConverter import ReversePolishNotationConverter
 
 class NodeParserTests(unittest.TestCase):
 
     def setUp(self):
-        self.mock = Mock()
-        self.parser = NodeParser(self.mock)
+        self.mock = mox.Mox()
+        self.rpnMock = self.mock.CreateMock(ReversePolishNotationConverter)
+        self.parser = NodeParser(self.rpnMock)
 
     def parse_expression(self, exp, rpn_exp):
-        self.mock.transform.return_value = rpn_exp
+        self.rpnMock.transform(exp).AndReturn(rpn_exp)
+        self.mock.ReplayAll()
         tree = self.parser.parse(exp)
         return tree
 
     def test_parses_a_plus(self):
         tree = self.parse_expression("1+2", "1 2 +")
         
-        self.mock.transform.assert_called_once_with("1+2")
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.evaluate())
         self.assertEqual(3, tree.evaluate())
@@ -29,7 +31,6 @@ class NodeParserTests(unittest.TestCase):
     def test_parses_a_minus(self):
         tree = self.parse_expression("1-2", "1 2 -")
         
-        self.mock.transform.assert_called_once_with("1-2")
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.evaluate())
         self.assertEqual(-1, tree.evaluate())
@@ -37,7 +38,6 @@ class NodeParserTests(unittest.TestCase):
     def test_parses_a_multiplication(self):
         tree = self.parse_expression("2*3", "2 3 *")
         
-        self.mock.transform.assert_called_once_with("2*3")
         self.assertEqual(2, tree.left.evaluate())
         self.assertEqual(3, tree.right.evaluate())
         self.assertEqual(6, tree.evaluate())   
@@ -45,7 +45,6 @@ class NodeParserTests(unittest.TestCase):
     def test_parses_a_division(self):
         tree = self.parse_expression("1/2", "1 2 /")
         
-        self.mock.transform.assert_called_once_with("1/2")
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.evaluate())
         self.assertEqual(0.5, tree.evaluate())     
@@ -53,7 +52,6 @@ class NodeParserTests(unittest.TestCase):
     def test_parses_complex_no_brackets(self):
         tree = self.parse_expression("1+2/8", "1 2 8 / +")
                    
-        self.mock.transform.assert_called_once_with("1+2/8")
         self.assertEqual(1, tree.left.evaluate())
         self.assertEqual(2, tree.right.left.evaluate())
         self.assertEqual(8, tree.right.right.evaluate())
@@ -62,7 +60,6 @@ class NodeParserTests(unittest.TestCase):
     def test_parses_complex_with_brackets(self):
         tree = self.parse_expression("(2+7)*4", "2 7 + 4 *")
         
-        self.mock.transform.assert_called_once_with("(2+7)*4")
         self.assertEqual(2, tree.left.left.evaluate())
         self.assertEqual(7, tree.left.right.evaluate())
         self.assertEqual(4, tree.right.evaluate())
